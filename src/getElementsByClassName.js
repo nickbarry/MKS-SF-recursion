80acde4a5e12cb7a45da41033a8e599a60aa8b23
Nico Barry <niko.barry@gmail.com>
2016-04-23 15:58:25 -0700

    Implement function
    This was simpler than I expected it to be at first, and it worked on the first go (with the exception of one non-structural change)! It could probably be refactored some, but I'm pretty happy with where it is right now.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,20 +3,24 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// Functions/properties I can use: document.body; element.childNodes; element.classList
-
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className /* [,  */){
-    var elsWithClass = [];
-    var body = document.body;
-
-    // PSEUDOCODE:
-    // Create an internal recursive function that looks at a node or list. This function will examine a node/list.
-        // If it's a node:
-            // Check its class. If it has the right class, push it to the results array
-            // If it has children, convert the list to an array, and then run the array of children through the function.
-        // If it's an array: (just return if it's an empty array)
-            // Run the function on the the first element of the array, and on the slice of the rest of the array.
-
-    return elsWithClass;
+var getElementsByClassName = function(className /* [, obj]  */){
+    var obj =  arguments[1] || document.body; // If there's no arguments[1], then assume the user called the function, and start searching from the top (the body element)
+    if(undefined === obj.nodeType){ // obj is an array, and has no nodeType property
+        if(0 === obj.length) {return [];} // if it's an empty array, just return an empty array
+        if(1 === obj.length) {return getElementsByClassName(className, obj[0]);} // If it has only one element, process that element
+        return getElementsByClassName(className, obj[0]).concat(getElementsByClassName(className, obj.slice(1))); // Return results of first element concatenated with results of the rest of the array
+    }else if(1 === obj.nodeType){ // obj is a node of some sort
+        var classes = [].slice.call(obj.classList); // Get class list and convert to array
+        var childNodesArr = [].slice.call(obj.childNodes);
+        var validatedObj = -1 === classes.indexOf(className)
+            ? [] // If className isn't found in classes list, return an empty array
+            : [obj]; // Otherwise return an array with the object in it
+        var validatedChildren = childNodesArr // childNodesArr will be undefined if obj has no children
+            ? childNodesArr // return array of childNodesArr if there are any
+            : []; // otherwise return an empty array
+        return validatedObj.concat(getElementsByClassName(className, validatedChildren)); // concat the validated object (which may be an empty array) with the results from checking the element's children
+    }else { // If nodeType is 2, 3, or 8 (attribute node, text node, or comment node), it has no children or classes...
+        return []; // ...so return an empty array since the results of this function will be concatenated with other arrays
+    }
 };
