0bb51730486dc2ece4b167f095274f8585dc4f46
Nico Barry <niko.barry@gmail.com>
2016-04-22 18:27:46 -0700

    Plan out approach in pseudocode
    I've created the plan I'll follow to create the getElementsByClassName functionality.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,20 @@
 //   return document.getElementsByClassName(className);
 // };
 
+// Functions/properties I can use: document.body; element.childNodes; element.classList
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className /* [,  */){
+    var elsWithClass = [];
+    var body = document.body;
+
+    // PSEUDOCODE:
+    // Create an internal recursive function that looks at a node or list. This function will examine a node/list.
+        // If it's a node:
+            // Check its class. If it has the right class, push it to the results array
+            // If it has children, convert the list to an array, and then run the array of children through the function.
+        // If it's an array: (just return if it's an empty array)
+            // Run the function on the the first element of the array, and on the slice of the rest of the array.
+
+    return elsWithClass;
 };
